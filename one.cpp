f56b6fb3292237d9069c8be62b5de3e03f7d307c
Lmiy <lmiy.xy@qq.com>
2018-12-09 11:12:16 +0800

    the first one

---
+++ one.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "this is the first one test !" << _FILE_ << endl;
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
